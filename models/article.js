47bbe65dc5b678f0ad26abcf0199ba88ceb720bb
DARKRIDER-WEB <66077868+DARKRIDER-WEB@users.noreply.github.com>
2022-07-23 12:15:55 +0530

    Add files via upload

--- models/article.js
+++ models/article.js
@@ -5,7 +5,7 @@ const createDomPurify = require('dompurify')
 const { JSDOM } = require('jsdom')
 const dompurify = createDomPurify(new JSDOM().window)
 
-var articleSchema = new mongoose.Schema({
+const articleSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true
--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-const express=require('express')
-const articleRouter=require('./routes/article.js')
-const app=express();
+const express = require('express')
+const mongoose = require('mongoose')
 const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
-const connectDB=require('./database/connection')
+const app = express()
 
-app.set('view engine','ejs')
+const port =process.env.PORT || 3000;
+
+mongoose.connect('mongodb+srv://blogapp:blogapp07@cluster0.dw8qnc0.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
-connectDB();
-// app.get('/',(req,res)=>{
-//     const articles=[{
-// title:'test article',
-// createdAt:new Date(),
-// description:'test description'
-// },
-// {title:'test article2',
-//     createdAt:new Date(),
-//     description:'test description2'
-//     }]
-//     res.render('articles/index.ejs',{articles:articles})
-// })
 app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({ createdAt: 'desc' })
-    res.render('articles/index', { articles: articles })
-  })
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-app.use('/articles',articleRouter)
-app.listen(3000)
+app.use('/articles', articleRouter)
+
+app.listen(port,()=>{
+  console.log("listening at ",port);
+})
